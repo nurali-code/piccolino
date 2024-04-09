256decf7fc1c2c47967bf20fb5d72e6c6684280f
nurali-code <nur3.dav.97@gmail.com>
2024-04-09 14:41:19 +0500

    why done

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ $('.reviews-slider').slick({
     variableWidth: true,
     slidesToScroll: 1
 });
+
+$('.dropdown-btn').on('click', function (e) {
+    var dCont = $(this).next('.dropdown-content');
+    var dParent = $(this).parents()[1];
+    $(dParent).find('.dropdown-btn').not($(this)).removeClass('is_active');
+    $(this).addClass('is_active');
+    $(dParent).find('.dropdown-content').not($(dCont)).slideUp();
+    $(this).next('.dropdown-content').slideDown();
+})
